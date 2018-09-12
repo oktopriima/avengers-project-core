613096a24ccf43906c8ae45a45bcfca3c1dc5b64
Okto Prima Jaya <octoprima93@gmail.com>
2018-09-13 00:19:54 +0700

    Change null string

--- model/users.go
+++ model/users.go
@@ -1,13 +1,17 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/oktopriima/go-package/library"
+)
 
 type Users struct {
-	ID            int       `json:"id"`
-	Name          string    `json:"name"`
-	Email         string    `json:"email"`
-	Password      string    `json:"-"`
-	RememberToken string    `json:"remember_token"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	ID            int                `json:"id"`
+	Name          string             `json:"name"`
+	Email         string             `json:"email"`
+	Password      string             `json:"-"`
+	RememberToken library.NullString `json:"remember_token"`
+	CreatedAt     time.Time          `json:"created_at"`
+	UpdatedAt     time.Time          `json:"updated_at"`
 }
